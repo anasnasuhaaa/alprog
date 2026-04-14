a76a2cd33a9a657af0f17bb9af8f5aa5aa2efe91
Anas Nasuha <infoanaspedia@gmail.com>
2026-04-15 00:39:19 +0700

    add matrix 2d

--- kuliah/minggu8/array2d.c
+++ kuliah/minggu8/array2d.c
@@ -23,5 +23,39 @@ int main()
     printf("%d ", revArr[i]);
   }
 
+  // Array 2D
+  // int row, col;
+
+  int arr2d[][3] = {{1, 2, 3}, {3, 2, 1}};
+  printf("Di bawah ini adalah penerapan Array 2D: \n");
+  printf("%d\n", arr2d[0][0]);
+  printf("%d\n", arr2d[0][1]);
+  printf("%d\n", arr2d[1][0]);
+
+  int row, col;
+  printf("Masukkan jumlah baris dan kolom: ");
+  scanf("%d %d", &row, &col);
+
+  int matrix[row][col];
+  for (int i = 0; i < row; i++)
+  {
+    for (int j = 0; j < col; j++)
+    {
+      scanf("%d", &matrix[i][j]);
+    }
+  }
+
+  for (int i = 0; i < row; i++)
+  {
+    for (int j = 0; j < col; j++)
+    {
+      printf("%d", matrix[i][j]);
+      if (col - 1 == j)
+        printf("\n");
+      else
+        printf(" ");
+    }
+  }
+
   return 0;
 }
